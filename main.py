9b3b0889c0ae68d00114e201cc5f42c41f19f2ba
argentum <princeofpersiaa3.ag@gmail.com>
2017-08-15 09:59:15 +0530

    few changes

--- main.py
+++ main.py
@@ -2,29 +2,37 @@ from os import listdir
 from os.path import isfile, join
 import argparse
 from music import MusicApp
-
+from shutil import copyfile
 
 class Stuff:
     def __init__(self):
         hlpTxt = 'provide music directory, defaults ~/Music'
         parser = argparse.ArgumentParser()
         parser.add_argument('--dir', nargs='+', required=False, default=[], help=hlpTxt)
-        args = parser.parse_args()
-        self.dirs = args.dir
+        parser.add_argument('--playlist', required=False, help='Path to playlist file')
+        self.args = parser.parse_args()
 
     def run(self):
         paths = []
-        for path in self.dirs:
+
+        if self.args.playlist:
+            copyfile(self.args.playlist, "playlist.txt")
+            return
+
+        if len(self.args.dir) == 0:
+            return
+
+        for path in self.args.dir:
             filesindir = [path + "/" + f for f in listdir(path) if isfile(join(path, f)) and f.endswith(('.mp3', 'wav'))]
             paths.extend(filesindir)
 
-        with open("songs.txt", "w+") as file:
+        with open("playlist.txt", "w+") as file:
             for path in paths:
                 file.write(path + "\n")
 
-        MusicApp().run()
 
 
 if __name__ == '__main__':
     s = Stuff()
     s.run()
+    MusicApp().run()
